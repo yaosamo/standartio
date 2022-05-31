39f828d84ed4bb62373e57bc7600f4a87ef1efbd
Yaroslav Samoylov <yaosamo@gmail.com>
2022-05-30 21:49:44 -0700

    works now, i think

--- components/iconsmapping.js
+++ components/iconsmapping.js
@@ -5,19 +5,20 @@ import styles from "../styles/Home.module.css";
 const size = 24;
 
 export default function Icons(props) {
-  IconsData.sort((a, b) => a.name > b.name);
+  const IconsSorted = []
+    .concat(IconsData)
+    .sort((a, b) => (a.name > b.name ? 1 : -1));
+  // IconsData.sort((a, b) => a.name > b.name);
 
-  return IconsData.map(function (Icon) {
-    return (
-      <div className={styles.iconshape} key={Icon.name}>
-        <Image
-          src={Icon.url}
-          alt={Icon.name}
-          width={size * props.ratio}
-          height={size * props.ratio}
-        />
-        <a className={styles.label}>{Icon.name}</a>
-      </div>
-    );
-  });
+  return IconsSorted.map((Icon, i) => (
+    <div className={styles.iconshape} key={i}>
+      <Image
+        src={Icon.url}
+        alt={Icon.name}
+        width={size * props.ratio}
+        height={size * props.ratio}
+      />
+      <a className={styles.label}>{Icon.name}</a>
+    </div>
+  ));
 }
