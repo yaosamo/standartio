f72118e6e54bc175b0eb4df598a8c638d7d880bd
Yaroslav Samoylov <yaosamo@gmail.com>
2022-06-21 13:26:05 -0700

    finally did it, took several days to figure out simple thing, thanks to my friends again

---
+++ components/icon.js
@@ -0,0 +1,34 @@
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const size = 24;
+
+export default function IconLogic({ Icon, i, Ratio }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  function handleMouseEnter() {
+    setIsAnimating(true);
+  }
+
+  function endAnimation() {
+    setIsAnimating(false);
+  }
+
+  return (
+    <a href={Icon.url} filename="image.svg" key={i} download={Icon.name}>
+      <div className={styles.iconshape} onMouseEnter={() => handleMouseEnter()}>
+        <Image
+          aria-label="Download Icon"
+          className={isAnimating ? styles.iconAnimated : ""}
+          src={Icon.url}
+          alt={Icon.name}
+          width={size * Ratio}
+          height={size * Ratio}
+          onAnimationEnd={() => endAnimation()}
+        />
+        <p className={styles.label}>{Icon.name}</p>
+      </div>
+    </a>
+  );
+}
--- components/iconslist.js
+++ components/iconslist.js
@@ -1,23 +1,12 @@
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+import IconLogic from "../components/icon";
 
 const size = 24;
 
 export default function IconsList({ filteredIcons, ratio }) {
   return filteredIcons.map((Icon, i) => (
-    <a href={Icon.url} filename="image.svg" key={i} download={Icon.name}>
-      <div className={styles.iconshape}>
-        <Image
-          aria-label="Download Icon"
-          className={styles.icon}
-          src={Icon.url}
-          alt={Icon.name}
-          width={size * ratio}
-          height={size * ratio}
-        />
-        <p className={styles.label}>{Icon.name}</p>
-      </div>
-    </a>
+    <IconLogic Icon={Icon} key={i} Ratio={ratio} />
   ));
 }
