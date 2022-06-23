f30596fdf6c9f749912ae0d5c912588d4b2a896f
Yaroslav Samoylov <yaosamo@gmail.com>
2022-06-22 22:38:52 -0700

    widget separated into files, dark mode on

--- components/emptyresults.js
+++ components/emptyresults.js
@@ -1,6 +1,11 @@
 import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
 
 export default function EmptyResults(props) {
+  const [disable, setDisable] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
   const request = {
     Requested: props.iconrequested,
     "Created at": new Date(),
@@ -8,6 +13,8 @@ export default function EmptyResults(props) {
 
   function write() {
     // Add one line to the sheet
+    setDisable(true);
+
     fetch(
       "https://sheet.best/api/sheets/6af17142-ce25-4892-8aee-efcdf8445f4a",
       {
@@ -22,19 +29,29 @@ export default function EmptyResults(props) {
       .then((r) => r.json())
       .then((request) => {
         // The response comes here
-        console.log(request);
+        setSuccess(true);
       })
       .catch((error) => {
         // Errors are reported there
         console.log(error);
+        setDisable(false);
+        setError(true);
       });
   }
   return (
     <div className={styles.nothingFound}>
-      <h1>No icons found </h1>
-      <p>Or maybe you want to submit request? It’s just one click.</p>
-      <button className={styles.requestButton} onClick={write}>
-        Please add <b>{props.iconrequested}</b> icon
+      <h1>No icons found. Want to submit request? It’s just one click.</h1>
+      <button
+        className={styles.requestButton}
+        onClick={write}
+        disabled={disable}
+      >
+        <b>
+          {!disable && "Please add " + props.iconrequested + " icon"}
+          {disable && !success && "Sending request"}
+          {success && "Thank you, we got your request!"}
+          {error && "Couldn't send. Try again."}
+        </b>
       </button>
     </div>
   );
---
+++ components/figmawidget.js
@@ -0,0 +1,34 @@
+import styles from "../styles/Home.module.css";
+
+export default function FigmaWidget() {
+  return (
+    <span className={styles.figma}>
+      <div className={styles.container}>
+        <model-viewer
+          camera-controls
+          src="../8.gltf"
+          alt="A 3D model of an astronaut"
+          camera-orbit="0deg 90deg 400%"
+          shadow-intensity="0"
+          auto-rotate
+          rotation-per-second="100deg"
+          max-field-of-view="100deg"
+          min-field-of-view="70deg"
+          max-camera-orbit="200deg"
+          interaction-prompt="none"
+        ></model-viewer>
+        <p>
+          Figma{" "}
+          <a
+            className={styles.link}
+            href="https://www.figma.com/community/file/1118765940009688786"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Library File
+          </a>
+        </p>
+      </div>
+    </span>
+  );
+}
