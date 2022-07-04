179dac46e2f51af2b079c739d81603e8ed279b43
Yaroslav Samoylov <yaosamo@gmail.com>
2022-07-03 23:18:08 -0700

    new widget

--- components/newiconswidget.js
+++ components/newiconswidget.js
@@ -1,13 +1,12 @@
 import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
 
 export default function NewIconsWidget(props) {
   return (
-    <div className={styles.ticker}>
+    <marquee className={styles.ticker}>
       <div className={styles.ticker_items_wrap}>
-        <div className={styles.ticker_item}>New Icons</div>
-        <div className={styles.ticker_item}>New Icons 2</div>
-        <div className={styles.ticker_item}>New Icons 3</div>
+        <div className={styles.ticker_item}>New Icons Added / 添加了新图标</div>
       </div>
-    </div>
+    </marquee>
   );
 }
