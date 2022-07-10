a7ad5fbcc7f2f59af88de06673d9f6e7b9c6e691
Yaroslav Samoylov <yaosamo@gmail.com>
2022-07-09 22:42:32 -0700

    New Icons

--- components/iconslist.js
+++ components/iconslist.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import IconLogic from "../components/icon";
+import IconStructure from "../components/icon";
 
 const size = 24;
 
 export default function IconsList({ filteredIcons, ratio }) {
   return filteredIcons.map((Icon, i) => (
-    <IconLogic Icon={Icon} key={i} Ratio={ratio} />
+    <IconStructure Icon={Icon} key={i} Ratio={ratio} />
   ));
 }
