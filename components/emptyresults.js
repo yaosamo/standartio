3d12b9d18ea897927fad812e17d17d6785354b23
Yaroslav Samoylov <yaosamo@gmail.com>
2022-07-11 11:25:02 -0700

    test

--- components/emptyresults.js
+++ components/emptyresults.js
@@ -1,11 +1,11 @@
-import styles from "../styles/Home.module.css";
 import { GoogleSpreadsheet } from "google-spreadsheet";
+import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 
-// process.env.GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, "\n");
 // Config variables
+// process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, "\n");
 const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
-const SHEET_ID = process.env.NEXT_PUBLIC_APP_SHEET_ID;
+const SHEET_ID = process.env.NEXT_PUBLIC_SHEET_ID;
 const CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
 const PRIVATE_KEY = process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY;
 
@@ -13,38 +13,32 @@ export default function EmptyResults(props) {
   const [disable, setDisable] = useState(false);
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+
   const request = {
     Requested: props.iconrequested,
     "Created at": new Date(),
   };
 
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
+  const accessSheet = async () => {
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_SERVICE_PRIVATE_KEY,
+    });
+    await doc.loadInfo();
+    console.log("doc title", doc.title);
+    const worksheet = doc.sheetsByIndex[0];
+    const rows = await worksheet.getRows();
+    rows.forEach((row) => {
+      console.log(row.Title);
+    });
+  };
+
   function write() {
-    setDisable(true);
-
-    const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
-
-    const appendSpreadsheet = async (row) => {
-      try {
-        await doc.useServiceAccountAuth({
-          client_email: CLIENT_EMAIL,
-          private_key: PRIVATE_KEY,
-        });
-        // loads document properties and worksheets
-        await doc.loadInfo();
-
-        const sheet = doc.sheetsById[SHEET_ID];
-        const result = await sheet.addRow(row);
-      } catch (e) {
-        console.error("Error: ", e);
-        setError(true);
-      }
-      setSuccess(true);
-    };
-
-    const newRow = { Requested: "new name", Value: "new value" };
-
-    appendSpreadsheet(newRow);
+    accessSheet();
   }
+
   return (
     <div className={styles.nothingFound}>
       <h1>No icons found. Want to submit request? It’s just one click.</h1>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
   const handleSizeChange = () => {
     setChecked(!checked);
   };
-  console.log("greeting:", process.env.NEXT_PUBLIC_GREETING);
 
   const [searchFieldValue, setSearchField] = useState("");
 
