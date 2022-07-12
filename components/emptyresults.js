4ecd8d4d278e9051753baed2f432713baefe65b3
Yaroslav Samoylov <yaosamo@gmail.com>
2022-07-11 22:29:27 -0700

    wow i did google auth

--- components/emptyresults.js
+++ components/emptyresults.js
@@ -2,41 +2,35 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 
-// Config variables
-// process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, "\n");
-const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
-const SHEET_ID = process.env.NEXT_PUBLIC_SHEET_ID;
-const CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
-const PRIVATE_KEY = process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY;
-
 export default function EmptyResults(props) {
   const [disable, setDisable] = useState(false);
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
 
-  const request = {
-    Requested: props.iconrequested,
-    "Created at": new Date(),
-  };
+  const doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_SPREADSHEET_ID);
 
-  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+  async function write() {
+    setDisable(true);
+    try {
+      await doc.useServiceAccountAuth({
+        client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY,
+      });
+      await doc.loadInfo();
+      console.log(doc.title);
 
-  const accessSheet = async () => {
-    await doc.useServiceAccountAuth({
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_SERVICE_PRIVATE_KEY,
-    });
-    await doc.loadInfo();
-    console.log("doc title", doc.title);
-    const worksheet = doc.sheetsByIndex[0];
-    const rows = await worksheet.getRows();
-    rows.forEach((row) => {
-      console.log(row.Title);
-    });
-  };
-
-  function write() {
-    accessSheet();
+      const sheet = doc.sheetsByIndex[0];
+      await sheet.addRow({
+        Requested: props.iconrequested,
+        "Created at": Date(),
+      });
+      setSuccess(true);
+    } catch (e) {
+      console.log("Here's what's wrong lol:", e);
+      setSuccess(false);
+      setError(true);
+      setDisable(false);
+    }
   }
 
   return (
