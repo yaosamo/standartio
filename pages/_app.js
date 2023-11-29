c4ec439fa3a6c41c382368a420c98a18817939f1
Yaroslav Samoylov <yaosamo@gmail.com>
2023-11-29 09:57:29 -0800

    analytics fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@ import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return;
-  <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
