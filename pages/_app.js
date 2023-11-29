f8a48b553eec4540bd2565c15c36fb92d93d93e3
Yaric Sam <yaosamo@gmail.com>
2023-11-29 09:53:34 -0800

    Revert "analytics"

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return;
-  <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
